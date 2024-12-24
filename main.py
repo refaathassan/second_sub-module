946741326d46eca5cdc9c211726e4d3889024245
Refaat Hassan <refathassan1289641@gmail.com>
2024-12-25 01:20:37 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+start_year=int(input("please enter the start year:   "))
+end_year=int(input("please enter the end year:   "))
+age=0
+if start_year< end_year :
+    age=end_year-start_year
+print("the age is ",age)
